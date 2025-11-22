9945d90312798620f4fb82be98fede6ac46d52a3
SuperFodder <tyler.d.hardy@hotmail.com>
2025-11-22 00:05:27 -0500

    Created player.py and circleshape.py, added asteroid and asteroidfield modules, and updated main.py and constants.py accordingly.

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import pygame
+import player
+import asteroid
+import asteroidfield
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
-pygame.init()
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    player.Player.containers = (updatable, drawable)
+    asteroid.Asteroid.containers = (updatable, drawable, asteroids)
+    asteroidfield.AsteroidField.containers = (updatable)
+    ship = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    field = asteroidfield.AsteroidField()
+    
     while True:
         log_state()
         for event in pygame.event.get():
@@ -18,7 +27,12 @@ def main():
                 pygame.quit()
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
+        print(f"Delta time: {dt:.4f} seconds")
 
 if __name__ == "__main__":
     main()
